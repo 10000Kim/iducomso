7fe295a6057053bfc1fdf2985768ccfa551ef331
10000Kim <kckm317@gmail.com>
2023-03-17 20:12:18 +0900

    Update part5.js

--- 3week/part5.js
+++ 3week/part5.js
@@ -1,6 +1,7 @@
 // 객체 리터럴 -> 리터럴: 소스코드에 고정된 값
 // 속성 표현 방식에 주목해야한다.
-var sayNode = function() {
+
+/** var sayNode = function() {
     console.log('Node');
   };
   var es = 'ES';
@@ -14,3 +15,20 @@ var sayNode = function() {
   oldObject.sayNode(); // Node
   oldObject.sayJS(); // JS
   console.log(oldObject.ES6); // Fantastic
+  */
+
+
+const sayNode = function() {
+    console.log('Node');
+  };
+  var es = 'ES';
+  var oldObject = {
+    sayJS: function() {
+      console.log('JS');
+    },
+    sayNode: sayNode,
+  };
+  oldObject[es + 2023] = 'Fantastic';
+  oldObject.sayNode(); // Node
+  oldObject.sayJS(); // JS
+  console.log(oldObject.ES2023); // Fantastic
