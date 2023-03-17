ae708a3c9168bd5b7e9682d902e2859ee7835b84
10000Kim <kckm317@gmail.com>
2023-03-17 20:14:18 +0900

    Update part5.js

--- 3week/part5.js
+++ 3week/part5.js
@@ -32,3 +32,8 @@ const sayNode = function() {
   oldObject.sayNode(); // Node
   oldObject.sayJS(); // JS
   console.log(oldObject.ES2023); // Fantastic
+
+  //객체 리터럴 -> 훨씬 간결한 문법으로 객체 리터럴 표현이 가능하다.
+/**
+ * 1. 객체의 메서드에 :function을 붙이지 않아도 됨
+ */
