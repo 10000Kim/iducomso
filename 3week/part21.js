a8df64a8302a3cf19d437af9e3b40ebac6217328
10000Kim <kckm317@gmail.com>
2023-03-17 21:54:28 +0900

    Create part21.js

---
+++ 3week/part21.js
@@ -0,0 +1,8 @@
+//async function 
+async function findAndSaveUser(Users) {
+    let user = await Users.findOne({});
+    user.name = 'zero';
+    user = await user.save();
+    user = await Users.findOne({ gender: 'm' });
+    // 생략
+  }
