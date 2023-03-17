89315bda833bbabf25ff025a1b7801d42d78965e
10000Kim <kckm317@gmail.com>
2023-03-17 20:29:30 +0900

    Create part7.js

---
+++ 3week/part7.js
@@ -0,0 +1,21 @@
+//화살표 함수
+/** 장점: function 이름이 없을때 (익명함수)는 일반적인 자바스크립트는 만들수 없다
+하지만 화살표 함수에서는 () => {    return }  형식으로 사용할 수 있다. */
+
+function add1(x, y) {
+    return x + y;
+  }
+  
+  const add2 = (x, y) => {
+    return x + y;
+  };
+  
+  const add3 = (x, y) => x + y;
+  
+  const add4 = (x, y) => (x + y);
+  
+  function not1(x) {
+    return !x;
+  }
+  
+  const not2 = x => !x;
