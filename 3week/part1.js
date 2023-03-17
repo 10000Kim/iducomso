8c1874e4c666b89ec0c48a0b2e4614de0504ad3b
10000Kim <kckm317@gmail.com>
2023-03-17 19:51:58 +0900

    Update part1.js

--- 3week/part1.js
+++ 3week/part1.js
@@ -3,9 +3,18 @@ if (true) {
     var x = 3;
 }
 console.log(x); //3
+/**
+  if (true) {
+    const y = 3;
+}
+console.log(y);
+ */
+
+// Uncaught ReferenceError : y is not defined
+// y 라는 변수가 선언이 안되어있음
 
 if (true) {
     const y = 3;
 }
+const y = 2;
 console.log(y);
-// Uncaught ReferenceError : y is not defined
