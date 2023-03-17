9014e943b1fa08c09eb430e6a32514782bfbad8f
10000Kim <kckm317@gmail.com>
2023-03-17 19:56:22 +0900

    Update part1.js

--- 3week/part1.js
+++ 3week/part1.js
@@ -13,11 +13,14 @@ console.log(y);
 // Uncaught ReferenceError : y is not defined
 // y 라는 변수가 선언이 안되어있음
 
-if (true) {
+/**
+ * if (true) {
     const y = 3;
 }
 const y = 2;
 console.log(y);
+ */
+
 
 /**
  * 1.상수에 할당한 값은 다른 값으로 변경 불가
@@ -25,3 +28,9 @@ console.log(y);
  * 3. 상수 선언 시부터 초기화가 필요함
  * 4. 초기화를 하지 않고 선언하면 에러
  */
+
+if (true) {
+    let y = 3;
+}
+let y = 2;
+console.log(y);
