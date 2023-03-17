25ed2e342bfc45218f44a370f8baf7942d80e37e
10000Kim <kckm317@gmail.com>
2023-03-17 21:37:58 +0900

    Create part15.js

---
+++ 3week/part15.js
@@ -0,0 +1,29 @@
+const condition = false; 
+const promise = new Promise((resolve, reject) => {
+    if (condition) {
+      resolve('성공');
+    } else {
+      reject('실패');
+    }
+  });
+promise
+  .then((message) => {
+    return new Promise((resolve, reject) => {
+      resolve(message);
+    });
+  })
+  .then((message2) => {
+    console.log(message2);
+    return new Promise((resolve, reject) => {
+      resolve(message2);
+    });
+  })
+  .then((message3) => {
+    console.log(message3);
+  })
+
+  .catch((error) => {
+    console.error(error);
+  });
+
+  //프로미스의 then 연달아 사용 O 프로미스 체이닝
