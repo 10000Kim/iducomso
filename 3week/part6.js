59ebdcc84b499e6ee9580e0db8f5e751f646b89a
10000Kim <kckm317@gmail.com>
2023-03-17 20:17:56 +0900

    Create part6.js

---
+++ 3week/part6.js
@@ -0,0 +1,14 @@
+const sayNode = function() {
+    console.log('Node');
+  };
+  var es = 'ES';
+const newObject = {
+    sayJS() {
+      console.log('JS');
+    },
+    sayNode,
+    [es + 6]: 'Fantastic',
+  };
+  newObject.sayNode(); // Node
+  newObject.sayJS(); // JS
+  console.log(newObject.ES6); // Fantastic
