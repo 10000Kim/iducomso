dd4b8df1c615ccb75bf671f551a04a872f3bf249
10000Kim <kckm317@gmail.com>
2023-03-24 20:11:40 +0900

    3.1 REPL 사용 실습

---
+++ 4week/3.2/helloWorld.js
@@ -0,0 +1,12 @@
+function helloWorld() {
+    console.log('Hello World');
+    helloNode();
+  }
+  
+  function helloNode() {
+    console.log('Hello Node');
+  }
+  
+  helloWorld();
+
+  //REPL 루프는 한번 돌기 때문에 결과값은 한번씩 나온다.
