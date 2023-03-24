944ad395561ff8f5c7ed7c8657c2e42fa56d9b98
10000Kim <kckm317@gmail.com>
2023-03-24 19:47:36 +0900

    3.3 모듈 만들기 실습

---
+++ 4week/3.3/func.js
@@ -0,0 +1,15 @@
+const { odd, even } = require('./var'); //구조분해할당
+
+function checkOddOrEven(num) {
+  if (num % 2) { // 홀수면
+    return odd;
+  }
+  return even;
+}
+
+// module.import ={
+//     odd,
+//     even
+// };
+
+module.exports = checkOddOrEven; //외부에서 사용할려면 (함수만했다)
