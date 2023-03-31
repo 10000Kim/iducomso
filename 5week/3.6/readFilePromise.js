841c8cc5b103cf7456caa6fdc8e1f86ff10b0d4e
10000Kim <kckm317@gmail.com>
2023-03-31 19:40:19 +0900

    3.6 파일 시스템 (fs 프로미스)

--- 5week/3.6/readFilePromise.js
+++ 5week/3.6/readFilePromise.js
@@ -1,6 +1,7 @@
 const fs = require('fs').promises;
 
-fs.readFile('./readme.txt')
+fs.readFile('/Users/man/Documents/GitHub/iducomso/5week/3.6/readme.txt')
+//맥에서는 절대경로를 주어야한다.
   .then((data) => {
     console.log(data);
     console.log(data.toString());
