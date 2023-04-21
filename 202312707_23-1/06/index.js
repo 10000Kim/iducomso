253dea7f072533eae63070c1f43431f4007bfcc3
Kim10000 <kckm317@gmail.com>
2023-04-21 22:22:55 +0900

    06번

---
+++ 202312707_23-1/06/index.js
@@ -0,0 +1,14 @@
+const answer = `npm은 Node Package Manager의 약어로 노드 패키지 매니저를 의미합니다. 노드가 자바스크립트
+프로그램을 컴퓨터에서도 실행 할 수 있게 해주는데 대부븐의 자바스크립트 프로그램은 패키지라는 이름으로
+npm에 등록되어 있으므로 특정 기능을 하는 패키지가 필요하다면 npm을 설치하면 됩니다.
+npme대체자로 yarn-> 페이스북이 내놓은 패키지 매니저로 리엑트나 리엑트 네이티브 같은 프레임워크를 사용할 때 사용합니다.
+pnpm은 npm의 성능을 개선한 패키지 매니저입니다.
+1.package name : 패키지 이름
+2.version : 패키지 버전
+3.entry point:  자바스크립트 실행파일 진입점
+4.test command: 코드를 테스트할때 입력할 명령어
+5.git repository: 코드를 저장해둔 깃 저장소 주소
+6.keywords: 패키지를 쉽게 찾을수 있게 해준다.
+7.license: 해당 패키지 라이선스를 넣는다.
+`;
+console.log(answer);
